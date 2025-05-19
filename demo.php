26f148dbcb45276d560b2e37758a854ec6e646a7
Cedric Joshua Palapuz <115864258+Corelocked@users.noreply.github.com>
2025-05-20 06:26:12 +0800

    removed comments

--- demo.php
+++ demo.php
@@ -1,53 +1,53 @@
 <?php
 
-// // odd or even
+// odd or even
 
-// $num = readline("enter a number");
-// $result = "Even";
-// if ($num % 2 != 0){
-//     $result = "Odd";
-// }
-// echo "The number is: {$result}";
+$num = readline("enter a number");
+$result = "Even";
+if ($num % 2 != 0){
+    $result = "Odd";
+}
+echo "The number is: {$result}";
 
-// // odd or even
+// odd or even
 
-// $num = readline("enter a number");
-// if($num % 2 == 0){
-//     echo $num . " is Even\n";
-// } else {
-//     echo $num . " is Odd";
-// }
+$num = readline("enter a number");
+if($num % 2 == 0){
+    echo $num . " is Even\n";
+} else {
+    echo $num . " is Odd";
+}
 
-// // prime numbers
+// prime numbers
 
-// function isPrime($num){
-//     if($num<2){
-//         return "{$num} is not a prime\n";
-//     }
-//     if ($num == 2){
-//         return "{$num} is a prime\n";
-//     }
-//     if($num % 2 == 0){
-//         return "{$num} is not a prime\n";
-//     }
-//     $sqrtNum = sqrt($num);
-//     for($i=3; $i <= $sqrtNum; $i +=2){
-//         if($num % $i == 0){
-//             return "{$num} is not a prime\n";
-//         }
-//     }
-//     return "{$num} is a prime\n";
-// }
+function isPrime($num){
+    if($num<2){
+        return "{$num} is not a prime\n";
+    }
+    if ($num == 2){
+        return "{$num} is a prime\n";
+    }
+    if($num % 2 == 0){
+        return "{$num} is not a prime\n";
+    }
+    $sqrtNum = sqrt($num);
+    for($i=3; $i <= $sqrtNum; $i +=2){
+        if($num % $i == 0){
+            return "{$num} is not a prime\n";
+        }
+    }
+    return "{$num} is a prime\n";
+}
 
-// $num = readline("enter a number");
-//     if(!is_numeric($num)){
-//         echo "Invalid input";
-//         exit;
-//     }
-// $num = (int)$num;
-// for ($ctr=0; $ctr<=$num; $ctr++){
-//     echo isPrime($ctr);
-// }
+$num = readline("enter a number");
+    if(!is_numeric($num)){
+        echo "Invalid input";
+        exit;
+    }
+$num = (int)$num;
+for ($ctr=0; $ctr<=$num; $ctr++){
+    echo isPrime($ctr);
+}
 
 require __DIR__ . '/vendor/autoload.php';
 use Cocur\Slugify\Slugify;
