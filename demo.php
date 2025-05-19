137d2698951d2acb80735b66bb6200128a68b7a0
Cedric Joshua Palapuz <115864258+Corelocked@users.noreply.github.com>
2025-05-20 06:02:11 +0800

    fixed the code for faker

--- demo.php
+++ demo.php
@@ -1,5 +1,6 @@
 <?php
-// odd or even
+
+// // odd or even
 // $num = readline("enter a number");
 // $result = "Even";
 // if ($num % 2 != 0){
@@ -7,8 +8,8 @@
 // }
 // echo "The number is: {$result}";
 
-// <?php
 // // odd or even
+
 // $num = readline("enter a number");
 // if($num % 2 == 0){
 //     echo $num . " is Even\n";
@@ -16,36 +17,36 @@
 //     echo $num . " is Odd";
 // }
 
-//prime numbers
-function isPrime($num){
-    if($num<2){
-        return "{$num} is not a prime\n";
-    }
-    if ($num == 2){
-        return "{$num} is a prime\n";
-    }
-    if($num % 2 == 0){
-        return "{$num} is not a prime\n";
-    }
-    $sqrtNum = sqrt($num);
-    for($i=3; $i <= $sqrtNum; $i +=2){
-        if($num % $i == 0){
-            return "{$num} is not a prime\n";
-        }
-    }
-    return "{$num} is a prime\n";
+// // prime numbers
 
-}
+// function isPrime($num){
+//     if($num<2){
+//         return "{$num} is not a prime\n";
+//     }
+//     if ($num == 2){
+//         return "{$num} is a prime\n";
+//     }
+//     if($num % 2 == 0){
+//         return "{$num} is not a prime\n";
+//     }
+//     $sqrtNum = sqrt($num);
+//     for($i=3; $i <= $sqrtNum; $i +=2){
+//         if($num % $i == 0){
+//             return "{$num} is not a prime\n";
+//         }
+//     }
+//     return "{$num} is a prime\n";
+// }
 
-$num = readline("enter a number");
-    if(!is_numeric($num)){
-        echo "Invalid input";
-        exit;
-    }
-$num = (int)$num;
-for ($ctr=0; $ctr<=$num; $ctr++){
-    echo isPrime($ctr);
-}
+// $num = readline("enter a number");
+//     if(!is_numeric($num)){
+//         echo "Invalid input";
+//         exit;
+//     }
+// $num = (int)$num;
+// for ($ctr=0; $ctr<=$num; $ctr++){
+//     echo isPrime($ctr);
+// }
 
 require __DIR__ . '/vendor/autoload.php';
 use Cocur\Slugify\Slugify;
